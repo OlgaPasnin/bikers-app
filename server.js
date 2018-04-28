51a58e7f8a9cfd756d36187e1cb587ccea077177
Olga Pasnin <olga@olgas-mbp.home>
2018-04-28 21:04:00 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,315 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors')
+const uuidv1 = require('uuid/v1');
+const app = express();
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+
+const cookieSession = require('cookie-session')
+
+app.use(cookieSession({
+  name: 'session',
+  keys: [uuidv1()],
+
+  // Cookie Options
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
+app.use(cors())
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/bikersApp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Database conntected!')
+});
+
+//SCHEMA
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  clanName: {
+    type: String,
+    required: false
+  },
+  motorbikeType: {
+    type: String,
+    required: false
+  }
+});
+
+var ClanSchema = new Schema({
+  clanName: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  clanImage: {
+    type: String,
+    unique: true,
+    required: false
+  },
+  clanMembers: {
+    type: Array,
+    required: false,
+  }
+});
+
+var LocationSchema = new Schema({
+  locationName: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  locationMembers: {
+    type: Array,
+    unique: true,
+    required: false
+  },
+  locationClans: {
+    type: Array,
+    unique: true,
+    required: false
+  }
+});
+
+UserSchema.pre('create', function(next) {
+    var user = this;
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password using our new salt
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+var User = mongoose.model('User', UserSchema);
+module.exports = User;
+var Clan = mongoose.model('Clan', ClanSchema);
+module.exports = Clan;
+var LocationName = mongoose.model('Location', LocationSchema);
+module.exports = LocationName;
+
+app.post('/register', function(req, res){
+
+  if (req.body.email &&
+    req.body.username &&
+    req.body.password &&
+    req.body.clanName &&
+    req.body.motorbikeType) {
+
+    let userData = {
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      clanName: req.body.clanName,
+      motorbikeType: req.body.motorbikeType
+    }
+    //use schema.create to insert data into the db
+    User.create(userData, function (err, user) {
+        if (err) return res.status(500).send(err);
+        res.sendStatus(201);
+    });
+  }
+});
+
+app.post('/login', function(req, res){
+  if(req.session.email){
+    console.log("User already logged in: " + req.session.email)
+    return res.sendStatus(208)
+  }
+  if (
+    req.body.email &&
+    req.body.password) {
+    let userData = {
+      email: req.body.email,
+      password: req.body.password
+    }
+
+    let queryObject = {};
+    queryObject.email = userData.email
+    //use schema.create to insert data into the db
+    User.findOne(queryObject).exec(function (err, user) {
+        if (err) return res.status(500).send(err);
+        user.comparePassword(userData.password, function(err, isMatch){
+          if (err) return res.status(500).send(err);
+          if (isMatch){
+            req.session.email = userData.email;
+            res.sendStatus(200);
+          } else {
+            return res.sendStatus(403);
+          }
+        })
+    });
+  }
+});
+
+app.get('/logout',function(req,res){
+    req.session = null;
+    res.sendStatus(200);
+
+});
+
+app.get('/login/:id?', function(req, res){
+  var queryObj = {};
+  if (req.params.id){
+    queryObj._id = req.params.id
+  }
+  User.find(queryObj).exec(function(err, user){
+    if (err) return res.status(500).send(err);
+    res.status(200).json(user);
+  });
+});
+
+
+//hashing a password before saving it to the database
+UserSchema.pre('save', function (next) {
+  var user = this;
+  bcrypt.hash(user.password, 10, function (err, hash){
+    if (err) {
+      return next(err);
+    }
+    user.password = hash;
+    next();
+  })
+});
+
+
+
+app.post('/createClan', function(req, res){
+  if(req.session.username === "admin"){
+    if (req.body.clanName &&
+      req.body.clanImage &&
+      req.body.clanMembers) {
+
+      let clanData = {
+        clanName: req.body.clanName,
+        clanImage: req.body.clanImage,
+        clanMembers: req.body.clanMembers
+      }
+      //use schema.create to insert data into the db
+      Clan.create(clanData, function (err, clan) {
+          if (err) return res.status(500).send(err);
+          res.sendStatus(201);
+      });
+    }
+  }else {
+    return res.sendStatus(403);
+  }
+});
+
+app.post('/checkin', function(req, res){
+    if (req.body.locationName &&
+      req.body.locationMembers ) {
+
+      let locationData = {
+        locationName: req.body.locationName,
+        locationMembers: req.body.locationMembers,
+      }
+      //use schema.create to insert data into the db
+      LocationName.create(locationData, function (err, clan) {
+          if (err) return res.status(500).send(err);
+          res.sendStatus(201);
+      });
+    }
+});
+
+
+
+
+
+
+
+//API GET REQUEST
+app.get('/dress/:id?', function(req, res){
+  var queryObj = {};
+  if (req.params.id){
+    queryObj._id = req.params.id
+  }
+  Dress.find(queryObj).exec(function(err, dress){
+    if (err) return res.status(500).send(err);
+    res.status(200).json(dress);
+  });
+});
+
+//API POST REQUEST
+app.post('/dress', function(req, res){
+  console.log(req.body);
+  var dressData = req.body;
+  var newDress =  new Dress(dressData);
+  newDress.save(function(err, dress){
+    if (err) return res.status(500).send(err);
+    res.sendStatus(201);
+  });
+});
+
+//API PUT REQUEST
+app.put('/dress/:id', function(req, res){
+  var updateableDressId = req.params.id;
+  Dress.update({ _id: updateableDressId }, req.body, function (err, raw) {
+      if (err) return handleError(err);
+      if(raw.nModified === 0 ) return res.sendStatus(404);
+      console.log('The raw response from Mongo was ', raw);
+      return res.sendStatus(200);
+  });
+});
+
+//API DELETE REQUEST
+app.delete('/dress/:id', function(req, res){
+  console.log('Dress to be deleted: ', req.params.id);
+  var deletableDressId = req.params.id;
+  Dress.remove({_id: deletableDressId}, function(err, deletedDress){
+    if(err) return res.status(500).send(err);
+    res.status(204).json(deletedDress);
+  });
+
+});
+
+
+app.listen(3000, function(){
+  console.log("Server listening");
+});
